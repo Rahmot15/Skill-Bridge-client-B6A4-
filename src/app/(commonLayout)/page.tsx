19e7f2131470f0d72e047e2f233bb751db644f36
Rahmot15 <mdrahmatulla666@gmail.com>
2026-07-17 18:25:15 +0600

    feat: add HowItWorks and Stats sections to home page (8 total sections)

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -2,16 +2,19 @@ import CTA from "@/components/modules/HomePages/CTA";
 import FeaturedTutors from "@/components/modules/HomePages/featured-tutors";
 import Features from "@/components/modules/HomePages/Features";
 import Hero from "@/components/modules/HomePages/Hero";
+import HowItWorks from "@/components/modules/HomePages/HowItWorks";
 import Pricing from "@/components/modules/HomePages/Pricing";
+import Stats from "@/components/modules/HomePages/Stats";
 import Testimonials from "@/components/modules/HomePages/Testimonials";
 
   export default async function Home() {
   return (
     <div className="">
       <Hero/>
+      <Stats/>
       <FeaturedTutors />
       <Features/>
-      {/* <Courses/> */}
+      <HowItWorks/>
       <Pricing/>
       <Testimonials/>
       <CTA/>
